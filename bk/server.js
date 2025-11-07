d47cfef13d381d20fbec1b43aedd24c9abb6f0b0
Scvotr <stasforeva23@mail.ru>
2025-11-07 16:02:23 +0300

    Update server.js to use config instead of direct environment variables

--- bk/server.js
+++ bk/server.js
@@ -3,14 +3,14 @@
 const http = require("http");
 const { serverErrorHandler } = require("./serverErrorHandler");
 const { logger } = require("./utils/logger");
+const config = require("./config");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello World!");
 });
 
-const host = process.env.HOST || "localhost";
-const port = process.env.PORT || 3000;
+const { host, port } = config.server;
 
 const startServer = async () => {
   return new Promise((resolve, reject) => {
@@ -18,7 +18,9 @@ const startServer = async () => {
       .listen({ host, port })
       .on("listening", () => {
         const address = server.address();
-        logger.info(`Сервер запущен на адресе ${address.address}:${address.port}`)
+        logger.info(
+          `Сервер запущен на адресе ${address.address}:${address.port}`
+        );
         resolve();
       })
       .on("error", (error) => {
