cd0f371cb3edacc8c7bccf3667f5d6768a73644b
Scvotr <stasforeva23@mail.ru>
2025-11-14 10:05:29 +0300

    .

--- bk/routingHandlers/routesHandlers.js
+++ bk/routingHandlers/routesHandlers.js
@@ -7,7 +7,6 @@ const { authRoutesHandle } = require("../routesHandlers/auth/authRoutesHandle");
 const {
   devicesRoutesHandle,
 } = require("../routesHandlers/devices/devicesRoutesHandle");
-const { routeCache } = require("../middleware/routeCache");
 
 const handleFavicon = async (req, res) => {
   res.writeHead(204); // No Content - иконки нет, но это нормально
@@ -15,37 +14,10 @@ const handleFavicon = async (req, res) => {
 };
 
 const routesHandlers = [
-  { 
-    prefix: "/auth", 
-    handler: authRoutesHandle,
-    cache: {
-      enabled: true,
-      ttl: 1000 * 60 * 10 // 10 minutes for auth routes
-    }
-  },
-  { 
-    prefix: "/admin", 
-    handler: adminRoutesHandle,
-    cache: {
-      enabled: false // Admin routes typically shouldn't be cached
-    }
-  },
-  { 
-    prefix: "/devices", 
-    handler: devicesRoutesHandle,
-    cache: {
-      enabled: true,
-      ttl: 1000 * 60 * 5 // 5 minutes for device routes
-    }
-  },
-  { 
-    prefix: "/favicon.ico", 
-    handler: handleFavicon,
-    cache: {
-      enabled: true,
-      ttl: 1000 * 60 * 60 * 24 // Cache favicon for 24 hours
-    }
-  },
+  { prefix: "/auth", handler: authRoutesHandle },
+  { prefix: "/admin", handler: adminRoutesHandle },
+  { prefix: "/devices", handler: devicesRoutesHandle },
+  { prefix: "/favicon.ico", handler: handleFavicon },
 ];
 
 module.exports = { routesHandlers };
